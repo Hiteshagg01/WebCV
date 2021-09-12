5cbf8a7a3116d32b99ffe4706996d6badd6aa776
Hitesh Aggarwal <Hiteshagg01>
2021-09-12 12:54:40 +0530

    v:1.5

---
+++ function.js
@@ -0,0 +1,13 @@
+$('#dark-theme').on('click', ()=> {
+    $(document.body).css("background-color", "#fff")
+    $(document.a).css("color", "#111")
+    $(document.p).css("color", "#111")
+    $('.navbar').css("background-color", "#fff")
+    $('.title-h').css("color","#111")
+    $('.title-p').css("color","#111")
+    $('.title-img').css("background-color", "#fff")
+    $('.title-img-div').css("background-color", "#fff")
+    $('.tile-card').css("background-color", "#fff")
+    $('.social-icon').css("background-color", "#fff")
+
+});
